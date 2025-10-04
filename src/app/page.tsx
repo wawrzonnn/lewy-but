05972db1b3fcd317517a4e2248ee2d6447a904e6
wawrzon <norbert.wawrzonek@gmail.com>
2025-10-04 12:56:01 +0200

    chore: bump app

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+	return (
+		<div className={styles.page}>
+			<h1>SIEMA JESTEM LEWY BUT Z KALKUTY JESTEM GIERKĄ </h1>
+		</div>
+	)
+}
